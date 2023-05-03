c41741f9df7da92586a486c5568485048eef81fa
Eftychios Karagiorgis <eftichios1997@hotmail.com>
2023-05-03 23:20:02 +0100

    Add cart icon to navbar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,10 @@
 import NavbarTop from "./navbar-top"
 
-export default function Layout({ children }: {children: React.ReactNode}) {
-  return (
-    <>
-        <NavbarTop/>
-      <main>{children}</main>
-    </>
-  )
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <NavbarTop />
+            <main>{children}</main>
+        </>
+    )
 }
--- src/components/navbar-top.tsx
+++ src/components/navbar-top.tsx
@@ -1,4 +1,5 @@
 import { signIn, signOut, useSession } from "next-auth/react";
+import { BsCart2 } from "react-icons/bs"
 
 const AuthNavbar: React.FC = () => {
 
@@ -20,16 +21,27 @@ const AuthNavbar: React.FC = () => {
 
 export default function NavbarTop() {
     const { data: sessionData } = useSession();
-    return <div className="px-8 h-12 bg-slate-900 text-white flex items-center justify-between">
-        <div className="flex">Shirtly</div>
-        <div>
-            {sessionData && sessionData.user?.name}
-        </div>
-        <div className="flex">
-            <div className="mr-4">Cart</div>
+    let itemsInBasket = 0;
 
-            <AuthNavbar />
+    itemsInBasket = 3;
+    return <>
+        <div className="px-8 h-12 bg-slate-900 text-white flex items-center justify-between">
+            <div className="flex">Shirtly</div>
+            <div>
+                {sessionData && sessionData.user?.name}
+            </div>
+            <div className="flex">
+                {sessionData &&
+                    <div className="mr-4 flex items-center relative">
+                        {itemsInBasket != 0 &&
+                            <div className="text-sm font-bold absolute w-1 h-1 p-2 flex items-center justify-center top-[-1px] right-[-9px] rounded-full bg-violet-400 text-slate-700">
+                                {itemsInBasket}
+                            </div>}
+                        <BsCart2 className="z-10" />
+                    </div>}
+                <AuthNavbar />
+            </div>
         </div>
-    </div>
+    </>
 
 }
