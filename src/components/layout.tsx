12e6d1630e4b07d235471cc92c95342c056f1b47
Eftychios Karagiorgis <eftichios1997@hotmail.com>
2023-05-07 20:15:51 +0100

    Move metadata to layout page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,14 @@
+import Head from "next/head"
 import NavbarTop from "./navbar-top"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
+            <Head>
+                <title>Shirt Store</title>
+                <meta name="description" content="An online store for buying shirts" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <NavbarTop />
             <main className="h-full">{children}</main>
         </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,11 @@
 import { type NextPage } from "next";
-import Head from "next/head";
 
 import ShirtContainer from "~/components/shirt-container";
 
 const Home: NextPage = () => {
     return (
         <>
-            <Head>
-                <title>Shirt Store</title>
-                <meta name="description" content="An online store for buying shirts" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main className="h-full">
-                <ShirtContainer />                    
-            </main>
+            <ShirtContainer />
         </>
     );
 };
