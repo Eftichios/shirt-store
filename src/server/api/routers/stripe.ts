89957d6f4a6db8d36570e6bd073983431cef3a0a
Eftychios Karagiorgis <eftichios1997@hotmail.com>
2023-05-20 10:38:08 +0100

    Add order and order items entity to database

--- src/server/api/routers/stripe.ts
+++ src/server/api/routers/stripe.ts
@@ -23,7 +23,7 @@ export const stripeRouter = createTRPCRouter({
                 })
             )
         }))
-        .mutation(async ({ input }) => {
+        .mutation(async ({ input, ctx }) => {
             try {
                 const shirts = input.shirts;
                 const lineItems = shirts.map((shirt) => ({
@@ -31,7 +31,10 @@ export const stripeRouter = createTRPCRouter({
                         currency: 'gbp',
                         product_data: {
                             name: shirt.description,
-                            images: [shirt.url]
+                            images: [shirt.url],
+                            metadata: {
+                                shirtId: shirt.id
+                            }
                         },
                         unit_amount: shirt.price * 100
                     },
@@ -46,6 +49,37 @@ export const stripeRouter = createTRPCRouter({
                     cancel_url: `${origin}/?canceled=true`,
                     automatic_tax: { enabled: true },
                 });
+
+
+                const order = {
+                    amount: session.amount_total as number,
+                    currency: 'gbp',
+                    url: session.url as string,
+                    createdAt: session.created,
+                    status: 'PENDING',
+                }
+
+                //console.log(order);
+                //console.log(order.url, order.url.length);
+
+                const dbOrder = await ctx.prisma.order.create(
+                    { data: order }
+                );
+
+                console.log(`Order with id ${dbOrder.id} has been created.`);
+
+                const orderItems = lineItems.map((lineItem) => ({
+                    orderId: dbOrder.id,
+                    shirtId: lineItem.price_data.product_data.metadata.shirtId,
+                    quantity: lineItem.quantity
+                }))
+
+                const dbOrderItems = await ctx.prisma.orderItems.createMany({
+                    data: orderItems
+                })
+
+                console.log(`${dbOrderItems.count} order items have been created.`);
+
                 return { sessionId: session.id };
             } catch (err) {
                 console.log(err);
