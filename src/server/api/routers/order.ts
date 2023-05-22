c0c985244bb07684043a4a8300da518114d41da7
Eftychios Karagiorgis <eftichios1997@hotmail.com>
2023-05-22 16:09:50 +0100

    Include shirts when retrieving the orders

--- src/server/api/routers/order.ts
+++ src/server/api/routers/order.ts
@@ -5,15 +5,25 @@ import {
 
 export const orderRouter = createTRPCRouter({
     getAllOrders: protectedProcedure
-        .query(async ({ ctx} ) => {
+        .query(async ({ ctx }) => {
 
             const userId = ctx.session.user.id;
 
             const orders = await ctx.prisma.order.findMany({
-                where: { userId: userId},
-                include: {orderItems: true}
+                where: { userId: userId },
+                include: { orderItems: {select: {
+                    shirt: true,
+                    quantity: true
+                }} }
             })
 
-            return orders;
-        })
+            return orders.map(order => ({
+                ...order,
+                shirts: order.orderItems.map(orderItem => ({
+                    ...orderItem.shirt,
+                    quantity: orderItem.quantity
+                }))
+            }))
+                    
+        }),
 })
